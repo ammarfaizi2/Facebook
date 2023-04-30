d9b947f4d3713704d6ecc3d02eead7215c73a904
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2023-05-01 03:57:31 +0700

    fb: web: Create initial web API
    Initial interface for the web API in JSON format.
    
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

---
+++ web/public/api.php
@@ -0,0 +1,90 @@
+<?php
+
+require __DIR__."/../../vendor/autoload.php";
+
+use Facebook\Facebook;
+
+const JSON_FLAGS = JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT;
+
+function err(int $code, $msg)
+{
+	http_response_code($code);
+	echo json_encode(["code" => $code, "error" => $msg], JSON_FLAGS);
+}
+
+function res(int $code, $msg)
+{
+	http_response_code($code);
+	echo json_encode(["code" => $code, "res" => $msg], JSON_FLAGS);
+}
+
+function getTimelineYears(Facebook $fb)
+{
+	if (!isset($_GET["username"]) || !is_string($_GET["username"])) {
+		err(400, "Bad request: missing \"username\" string parameter");
+		return 0;
+	}
+
+	res(200, $fb->getTimelineYears($_GET["username"]));
+	return 0;
+}
+
+function getTimelinePosts(Facebook $fb)
+{
+	if (!isset($_GET["username"]) || !is_string($_GET["username"])) {
+		err(400, "Bad request: missing \"username\" string parameter");
+		return 0;
+	}
+
+	if (!isset($_GET["year"]) || !is_numeric($_GET["year"])) {
+		$year = -1;
+	} else {
+		$year = (int)$_GET["year"];
+	}
+
+	res(200, $fb->getTimelinePosts($_GET["username"], $year));
+	return 0;
+}
+
+function handle_action(Facebook $fb, string $action)
+{
+	switch ($action) {
+	case "getTimelineYears":
+		return getTimelineYears($fb);
+	case "getTimelinePosts":
+		return getTimelinePosts($fb);
+	default:
+		err(400, "Bad request: unknown action");
+		return 0;
+	}
+}
+
+function main()
+{
+	header("Content-Type: application/json");
+
+	require __DIR__."/../auth.php";
+	if (!isset($_GET["key"]) || $_GET["key"] !== $auth_key) {
+		err(401, "Unauthorized");
+		return 0;
+	}
+
+	if (!isset($_GET["action"]) || !is_string($_GET["action"])) {
+		err(400, "Bad request: missing \"action\" string parameter");
+		return 0;
+	}
+
+	$fb = new Facebook($session_dir);
+	$fb->setProxy($tor_proxy);
+	$fb->setBaseUrl("https://mbasic.facebookwkhpilnemxj7asaniu7vnjjbiltxjqhye3mhbshg7kx5tfyd.onion");
+	$fb->setCookieString($cookie);
+
+	try {
+		$ret = handle_action($fb, $_GET["action"]);
+	} catch (Exception $e) {
+		err(422, "Error: ".$e->getMessage());
+		return 0;
+	}
+}
+
+main();
