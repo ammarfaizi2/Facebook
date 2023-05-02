dabde77c9abe90a7407c46a1a45ef6a0b9b383d6
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2023-05-02 23:57:04 +0700

    fb: web: Provide a proxy to access onion endpoints
    The user may not have accees to the Tor network. Create a proxy endpoint
    to make the Facebook onion asset accessible from the outside world.
    
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- web/public/api.php
+++ web/public/api.php
@@ -85,30 +85,22 @@ function http_write_header_callback($curl, $data)
 	return $len;
 }
 
-function httpGet(Facebook $fb)
+function fb_http_get(Facebook $fb, string $url): int
 {
-	global $content_is_json;
-
-	if (!isset($_GET["url"]) || !is_string($_GET["url"])) {
-		err(400, "Bad request: missing \"url\" string parameter");
-		return 0;
-	}
-
-	$url = $_GET["url"];
 	$p = parse_url($url);
 	if ($p === false) {
 		err(400, "Bad request: invalid URL");
-		return 0;
+		return 1;
 	}
 
 	if ($p["scheme"] !== "https") {
 		err(400, "Bad request: invalid URL scheme (only HTTPS is allowed)");
-		return 0;
+		return 1;
 	}
 
 	if (!preg_match("/facebook|fbcdn/", $p["host"])) {
 		err(400, "Bad request: invalid URL host");
-		return 0;
+		return 1;
 	}
 
 	if (isset($_GET["follow_redirect"])) {
@@ -127,7 +119,20 @@ function httpGet(Facebook $fb)
 		],
 		"follow_redirect" => $follow_redirect,
 	]);
-	exit(0);
+	return 0;
+}
+
+function httpGet(Facebook $fb)
+{
+	global $content_is_json;
+
+	if (!isset($_GET["url"]) || !is_string($_GET["url"])) {
+		err(400, "Bad request: missing \"url\" string parameter");
+		return 0;
+	}
+
+	if (!fb_http_get($fb, $data))
+		exit(0);
 }
 
 function handle_action(Facebook $fb, string $action)
@@ -147,26 +152,96 @@ function handle_action(Facebook $fb, string $action)
 	}
 }
 
+function rewriteOnionURL(?string $str): ?string
+{
+	if (is_null($str))
+		return NULL;
+
+	$signature = md5($str.API_SECRET, true);
+
+	/**
+	 * Try to compress the URL. If the result is bigger, don't compress it.
+	 */
+	$str_comp = gzdeflate($str, 9);
+	if (strlen($str_comp) < $str) {
+		$str = $str_comp;
+		$is_compressed = "\1";
+	} else {
+		$is_compressed = "\0";
+	}
+
+	if (isset($_SERVER["HTTPS"])) {
+		$url = "https://";
+	} else {
+		$url = "http://";
+	}
+
+	if (isset($_SERVER["HTTP_HOST"])) {
+		$url .= $_SERVER["HTTP_HOST"];
+	} else {
+		$url .= "localhost";
+	}
+
+	$url .= "/api.php?u=";
+	return $url.urlencode(base64_encode($is_compressed.$signature.$str));
+}
+
+function handle_url_proxy(Facebook $fb, string $url)
+{
+	$url = @base64_decode($url);
+	if (!$url || strlen($url) < 17) {
+		err(404, "Not found");
+		return 0;
+	}
+
+	$is_compressed = $url[0];
+	$signature = substr($url, 1, 16);
+	$data = substr($url, 17);
+
+	if ($is_compressed) {
+		$data = gzinflate($data);
+	}
+
+	if ($signature !== md5($data.API_SECRET, true)) {
+		err(400, "Invalid signature");
+		return 0;
+	}
+
+	if (!fb_http_get($fb, $data))
+		exit(0);
+}
+
 function main()
 {
 	require __DIR__."/../auth.php";
-	if (!isset($_GET["key"]) || $_GET["key"] !== $auth_key) {
-		err(401, "Unauthorized");
-		return 0;
-	}
+	if (!isset($_GET["u"]) || !is_string($_GET["u"])) {
+		if (!isset($_GET["key"]) || $_GET["key"] !== $auth_key) {
+			err(401, "Unauthorized");
+			return 0;
+		}
 
-	if (!isset($_GET["action"]) || !is_string($_GET["action"])) {
-		err(400, "Bad request: missing \"action\" string parameter");
-		return 0;
+		if (!isset($_GET["action"]) || !is_string($_GET["action"])) {
+			err(400, "Bad request: missing \"action\" string parameter");
+			return 0;
+		}
+
+		$handle_url_proxy = false;
+	} else {
+		$handle_url_proxy = true;
 	}
 
 	$fb = new Facebook($session_dir);
+	$fb->registerRewriteURLCallback("rewriteOnionURL");
 	$fb->setProxy($tor_proxy);
 	$fb->setBaseUrl("https://mbasic.facebookwkhpilnemxj7asaniu7vnjjbiltxjqhye3mhbshg7kx5tfyd.onion");
 	$fb->setCookieString($cookie);
 
 	try {
-		$ret = handle_action($fb, $_GET["action"]);
+		if ($handle_url_proxy) {
+			$ret = handle_url_proxy($fb, $_GET["u"]);
+		} else {
+			$ret = handle_action($fb, $_GET["action"]);
+		}
 	} catch (Exception $e) {
 		err(422, "Error: ".$e->getMessage());
 		return 0;
