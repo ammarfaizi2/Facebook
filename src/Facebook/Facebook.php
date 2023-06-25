ammarfaizi2/Facebook

commit 88952f396b1b4831eab3b8ed5d71959e42686a88

fb: Introduce `getCache()` and `setCache()` functions
A preparation patch to implement better caching mechanism. All methods
that need cache will call these functions.

Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>
Link: https://lore.gnuweeb.org/gwml/20230509104658.70953-2-ammarfaizi2@gnuweeb.org
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

src/Facebook/Facebook.php
@@ -287,4 +287,49 @@ class Facebook
 
 		return $url;
 	}
+
+	/**
+	 * @param  string $key
+	 * @param  mixed  $data
+	 * @param  int    $expire
+	 * @return void
+	 */
+	private function setCache(string $key, $data, int $expire = 600): void
+	{
+		$key = str_replace(["/", "\\"], "_", $key);
+		$data = [
+			"exp"  => time() + $expire,
+			"data" => $data
+		];
+		$data = json_encode($data, JSON_INTERNAL_FLAGS);
+		if (!is_dir($this->cache_dir)) {
+			mkdir($this->cache_dir, 0777, true);
+			if (!is_dir($this->cache_dir)) {
+				throw new \Exception("Unable to create cache directory: {$this->cache_dir}");
+			}
+		}
+		file_put_contents("{$this->cache_dir}/{$key}.json", $data);
+	}
+
+	/**
+	 * @param  string $key
+	 * @return mixed
+	 */
+	private function getCache(string $key)
+	{
+		$key = str_replace(["/", "\\"], "_", $key);
+		$file = "{$this->cache_dir}/{$key}.json";
+
+		if (!file_exists($file)) {
+			return NULL;
+		}
+
+		$data = json_decode(file_get_contents($file), true);
+		if (!isset($data["exp"]) || !isset($data["data"])) {
+			unlink($file);
+			return NULL;
+		}
+
+		return $data["data"];
+	}
 }

commit c9d62a1087e10c05dae95944823018f1b739eb87

fb: Fix `getCache()` expiration logic handling
The getCache() function doesn't take the expiration date into account.
Make sure it does.

Fixes: 88952f396b1b4831eab3b8ed5d71959e42686a88 ("fb: Introduce `getCache()` and `setCache()` functions")
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

src/Facebook/Facebook.php
@@ -306,6 +306,11 @@ class Facebook
 			return NULL;
 		}
 
+		if ($data["exp"] < time()) {
+			unlink($file);
+			return NULL;
+		}
+
 		return $data["data"];
 	}
 